3ec751546b3e032f286e7f7c1d4147e0b2de8bff
itsinqz <nate.loeffel@gmail.com>
2022-07-24 13:56:42 -0700

    pushg

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -10,12 +10,6 @@ module.exports = {
 			);
 		}
 
-		const redRole = "948582612228771910";
-		const orangeRole = "948582618574778418";
-		const yellowRole = "948582618981617674";
-		const greenRole = "948582612933439578";
-		const blueRole = "948582618897731685";
-		const purpleRole = "948582612610482236";
 
 		async function handleInteraction() {
 			if (!interaction.inGuild()) return;
@@ -26,32 +20,10 @@ module.exports = {
 				await slashcmd.run({ client, interaction });
 			} else if (interaction.isSelectMenu()) {
 				if (interaction.customId == "colorselect") {
-					if (
-						interaction.member.roles.cache.hasAny(
-							redRole,
-							yellowRole,
-							blueRole,
-							greenRole,
-							purpleRole,
-							orangeRole
-						)
-					) {
-						await interaction.member.roles.remove([
-							redRole,
-							yellowRole,
-							blueRole,
-							greenRole,
-							purpleRole,
-							orangeRole,
-						]);
-					}
-					await interaction.deferReply({ ephemeral: true });
-					addNameRole(interaction.values[0]);
-					await interaction.editReply({
-						content: `You have been given the ${interaction.values[0]} color role.`,
-						ephemeral: true,
-					});
+					
 				}
+			} else if (interaction.isModalSubmit()) {
+				
 			}
 		}
 		handleInteraction();
--- slash/updateMembers.js
+++ slash/updateMembers.js
@@ -10,13 +10,12 @@ const {
     getMap,
     getGuildMembers,
     getDiscord,
-    addNameRole
+    addNameRole,
 } = require("../helpers");
-const { promisify } = require('util')
-const sleep = promisify(setTimeout)
+const { promisify } = require("util");
+const sleep = promisify(setTimeout);
 
 module.exports = {
-<<<<<<< HEAD
     data: new SlashCommandBuilder()
         .setName("updateallembers")
         .setDescription("Updates all the members.")
@@ -27,109 +26,55 @@ module.exports = {
                 .setRequired(true)
         ),
     run: async ({ client, interaction }) => {
-        // var roledUsers = []
-        // const doSleep = async () => {
-        //     await sleep(800)
-        //     //do stuff
-        // }
-        // const guildMemberRole = 1;
-        // const embed = new MessageEmbed()
-        //     .setTitle("Member list.")
-        //     .setDescription("List of all members in The Dawns Awakening.");
-        // memberUUIDSList = await getGuildMembers(interaction.options.getString("api-key"))
-        // for (var i = 0; i < memberUUIDSList.length ; i++) {
-        //     const discords = await getDiscord(memberUUIDSList[i], (interaction.options.getString("api-key")))
-        //     if (discords == false || discords == undefined) {console.log("Member with the UUID, ", memberUUIDSList[i], ",needs to link their discord.")}
-        //     else {
-        //     console.log(discords, " - ", i)
-        //     roledUsers.push(discords)
-        //     await doSleep()
-        //     }
-            
-        // }
-        const roledUsers = ["Darkr#4492", "9206#0149", "Inqz#9999"]
-        for (var i = 0; i < roledUsers.length; i++) {
-            try {
-                const rolename = "Guild Member" 
-                const themember = (await interaction.guild.members.cache.find((user) => user.username ==="Inqz#9999"))
-                // console.log(await interaction.guild.members.fetch(themember.id))
-                // console.log(themember)
-                (await interaction.guild.members.fetch({query: "Inqz#9999"})).roles.add(
-                    client.guilds.cache
-                        .get("605743544963170314")
-                        .roles.cache.find((role) => role.name === rolename)
+        var roledUsers = [];
+        const doSleep = async () => {
+            await sleep(800);
+            //do stuff
+        };
+        const guildMemberRole = "990040391548018688";
+        const embed = new MessageEmbed()
+            .setTitle("Member list.")
+            .setDescription("List of all members in The Dawns Awakening.");
+        var memberUUIDSList = await getGuildMembers(interaction.options.getString("api-key"));
+        console.log(await memberUUIDSList)
+        for (var i = 0; i < (await memberUUIDSList.length); i++) {
+            const discords = await getDiscord(
+                memberUUIDSList[i],
+                interaction.options.getString("api-key")
+            );
+            if (discords == false || discords == undefined) {
+                console.log(
+                    "Member with the UUID, ",
+                    memberUUIDSList[i],
+                    ",needs to link their discord."
                 );
-                console.log("Role added to - ", roledUsers[i])
-            } catch (err) {
-                console.log(err)
-                console.log("Role could not be added to user - ", roledUsers[i])
+            } else {
+                console.log(discords, " - ", i);
+                roledUsers.push(discords);
+                await doSleep();
             }
-            await sleep(1000)
         }
-        // for (var i = 0; i < roledUsers.length; i++) {
-        //     try {
-        //         const rolename = "Guild Member" 
-        //         const themember = await interaction.guild.members.cache.find((member) => member.user.tag === roledUsers[i])
-        //         await themember.roles.add(
-        //             client.guilds.cache
-        //                 .get("605743544963170314")
-        //                 .roles.cache.find((role) => role.name === rolename)
-        //         );
-        //         console.log("Role added to - ", roledUsers[i])
-        //     } catch (err) {
-        //         console.log(err)
-        //         console.log("Role could not be added to user - ", roledUsers[i])
-        //     }
-        //     await sleep(1000)
-        // }
-        return await interaction.editReply("Finished.");
-    },
-};
-=======
-	data: new SlashCommandBuilder()
-		.setName("updateallembers")
-		.setDescription("Updates all the members.")
-		.addStringOption((option) =>
-			option
-				.setName("api-key")
-				.setDescription("send the api key for the bot to use.")
-				.setRequired(true)
-		),
-	run: async ({ client, interaction }) => {
-		var roledUsers = []
-		const doSleep = async () => {
-			await sleep(800)
-			//do stuff
-		}
-		const guildMemberRole = "990040391548018688";
-		const embed = new MessageEmbed()
-			.setTitle("Member list.")
-			.setDescription("List of all members in The Dawns Awakening.");
-		memberUUIDSList = await getGuildMembers(interaction.options.getString("api-key"))
-		for (var i = 0; i < memberUUIDSList.length; i++) {
-			const discords = await getDiscord(memberUUIDSList[i], (interaction.options.getString("api-key")))
-			if (discords == false || discords == undefined) { console.log("Member with the UUID, ", memberUUIDSList[i], ",needs to link their discord.") }
-			else {
-				console.log(discords, " - ", i)
-				roledUsers.push(discords)
-				await doSleep()
-			}
+        
+            for (var i = 0; i < roledUsers.length; i++) {
+                try {
+                    const rolename = "Guild Member";
+                    const discordTag = await roledUsers[i];
+                    const discordObject = client.users.cache.find(
+                        (user) => user.tag === discordTag
+                    ) ?? "Not Found";
+                    await interaction.channel.send(discordObject);
+                    // await discordObject.roles.add(guildMemberRole);
+                    console.log("Role added to - ", roledUsers[i]);
+                } catch (err) {
+                    console.log(err);
+                    console.log(
+                        "Role could not be added to user - ",
+                        discordObject
+                    );
+                }
+                await sleep(700);
+            }
+            return await interaction.editReply("Finished.");
+        }
+    };
 
-			for (var i = 0; i < roledUsers.length; i++) {
-				try {
-					const rolename = "Guild Member"
-					const discordTag = await roledUsers[i]
-					const discordObject = bot.users.cache.find((user) => user.tag === discordTag);
-					await interaction.channel.send(discordObject)
-					// await discordObject.roles.add(guildMemberRole);
-					console.log("Role added to - ", roledUsers[i])
-				} catch (err) {
-					console.log(err)
-					console.log("Role could not be added to user - ", roledUsers[i])
-				}
-				await sleep(1000)
-			}
-			return await interaction.editReply("Finished.");
-		},
-	};
->>>>>>> bcd60a8c2aabd5ab5654f0220dbcb6ee71656e24
